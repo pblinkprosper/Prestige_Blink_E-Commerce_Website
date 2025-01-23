a2f5c58e17433b0687e6492ea7577f2600612d2e
pblinkprosper <125454681+pblinkprosper@users.noreply.github.com>
2025-01-23 13:32:34 +0100

    backend update

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url',
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+})
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'ecommerce-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+})
