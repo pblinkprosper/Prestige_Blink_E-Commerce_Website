b4351209e19d5f5ba397262fc1999a953e25e274
pblinkprosper <125454681+pblinkprosper@users.noreply.github.com>
2025-01-27 10:45:16 +0100

    frontend connection setup

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,6 +3,32 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if(isValid) {
+            uploadError = null
+        }
+      cb(uploadError, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const fileName = file.originalname.split(' ').join('-');
+      const extension = FILE_TYPE_MAP[file.mimetype];
+      cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+  })
+  
+  const uploadOptions = multer({ storage: storage })
 
 router.get(`/`, async (req, res) => {
     let filter = {};
@@ -29,28 +55,37 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
     try {
         const category = await Category.findById(req.body.category);
         if (!category) return res.status(400).send('Invalid Category');
     } catch (err) {
         return res.status(500).json({ message: err.message });
     }
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
 
-    let product = new Product(
-    {
-        name: req.body.name,
-        description: req.body.description,
-        richDescription: req.body.richDescription,
-        image: req.body.image,
-        brand: req.body.brand,
-        price: req.body.price,
-        category: req.body.category,
-        countInStock: req.body.countInStock,
-        rating: req.body.rating,
-        numReviews: req.body.numReviews,
-        isFeatured: req.body.isFeatured
-    })
+    const fileName = req.file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+    let product;
+    try {
+        product = new Product({
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: `${basePath}${fileName}`, //"http://localhost:3000/public/uploads/image-1631530130137.jpg"
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured
+        });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 
     try {
         product = await product.save();
@@ -129,5 +164,36 @@ router.get(`/get/featured/:count`, async (req, res) => {
     }
     res.send(products);
 })
+
+router.put(
+    '/gallery-images/:id', 
+    uploadOptions.array('images', 10), 
+    async (req, res) => {
+        if (!mongoose.isValidObjectId(req.params.id)) {
+            return res.status(400).send('Invalid Product Id');
+        }
+        const files = req.files;
+        let imagesPaths = [];
+        const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+        if(files) {
+            files.map(file => {
+                imagesPaths.push(`${basePath}${file.filename}`);
+            })
+        }
+
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                images: imagesPaths
+            },
+            {new: true}
+        )
+        if(!product)
+        return res.status(500).send('the product cannot be updated!')
+        
+        res.send(product);
+    }
+)
 
 module.exports = router;
