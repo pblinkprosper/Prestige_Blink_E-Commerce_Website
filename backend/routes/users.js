5c6af9699eb22cf2370b0e945c3a0f550d7dd02e
pblinkprosper <125454681+pblinkprosper@users.noreply.github.com>
2025-01-27 05:33:02 +0100

    Orders.js update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -99,6 +99,18 @@ router.post(`/register`, async (req, res) => {
     }
 })
 
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if(user) {
+            return res.status(200).json({success: true, message: 'the user is deleted!'})
+        } else {
+            return res.status(404).json({success: false, message: 'user@ not found!'})
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err})
+    })
+})
+
 router.get(`/get/count`, async (req, res) => {
     const userCount = await User.countDocuments((count) => count)
 
