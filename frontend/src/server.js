93d330dee5073d493ddf2119c7d5bd065b03eb89
pblinkprosper <125454681+pblinkprosper@users.noreply.github.com>
2025-01-27 11:12:07 +0100

    moved files

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,26 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/users', (req, res) => {
+    const users = [{
+        id: '123',
+        name: 'John'
+    }, {
+        id: '234',
+        name: 'Jane'
+    }, {
+        id: '345',
+        name: 'Jake'
+    }];
+
+    res.json(users);
+});
 
 app.listen(8080, () => {
     console.log('Server is listening on http://localhost:8080');
