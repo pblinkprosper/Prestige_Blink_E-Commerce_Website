33a582137744b9d2bc4859385beb4d0a723f0289
pblinkprosper <125454681+pblinkprosper@users.noreply.github.com>
2025-01-30 13:28:11 +0100

    Testing fetch methods to connect my frontend to the backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,25 +3,16 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/api/users', (req, res) => {
-    const users = [{
-        id: '123',
-        name: 'John'
-    }, {
-        id: '234',
-        name: 'Jane'
-    }, {
-        id: '345',
-        name: 'Jake'
-    }];
-
-    res.json(users);
-});
-
 app.listen(8080, () => {
     console.log('Server is listening on http://localhost:8080');
 });
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
+})
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+})
